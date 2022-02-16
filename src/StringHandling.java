e30a3dcba3188a483f1ca6146cdad4d9f9d5217c
manhhung012 <nhoxwiz87@gmail.com>
2022-02-16 17:04:41 +0700

    StringHanding

--- src/StringHandling.java
+++ src/StringHandling.java
@@ -15,8 +15,34 @@ public class StringHandling {
         System.out.println(str.contentEquals("PhamManhHung"));
         System.out.println(str.contentEquals("PhamManh"));
 
+        System.out.println(str.hashCode());
+
+        System.out.println(str.indexOf(65));
+        System.out.println(str.indexOf('n'));
+        System.out.println(str.indexOf("am"));
+        System.out.println(str.indexOf("ph"));
+
+        System.out.println(str.lastIndexOf("n"));
+
+        System.out.println(str.isEmpty());
+        System.out.println("".isEmpty());
+
+        System.out.println(str.length());
+        System.out.println(str.replace('a','u'));
+
+        System.out.println(str.startsWith("Pham"));
+        System.out.println(str.startsWith("ham"));
+
+        System.out.println(str.substring(4));
+        System.out.println(str.substring(4,5));
+
+        System.out.println("   Hung   ".trim());
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+
+        System.out.println(String.valueOf(123.3)+"0000");
 
 
-        
     }
 }
